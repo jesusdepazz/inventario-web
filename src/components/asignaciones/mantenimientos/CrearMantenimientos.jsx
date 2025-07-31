72bb620d2b47f24892bd5087fdb7ea4e3a1cdadc
jesusdepazz <rodolfomoralesdepaz03@gmail.com>
2025-07-31 15:53:03 -0600

    add commit "Cambios para usar servicios de endpoints"

--- src/components/asignaciones/mantenimientos/CrearMantenimientos.jsx
+++ src/components/asignaciones/mantenimientos/CrearMantenimientos.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import MantenimientosService from "../services/MantenimientosService";
+import MantenimientosService from "../../../services/MantenimientosServices";
 
 const CrearMantenimiento = () => {
   const [form, setForm] = useState({
