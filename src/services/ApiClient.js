322f97e6ce6ce32a2cbdae791b443503346da4f1
jesusdepazz <rodolfomoralesdepaz03@gmail.com>
2026-01-21 11:27:09 -0600

    "Cambiando vistas para que solo administradores puedan ver las opciones de menus"

--- src/services/ApiClient.js
+++ src/services/ApiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://inveq.guandy.com/api", 
+  baseURL: "https://inveq-test.guandy.com/api", 
   headers: {
     "Content-Type": "application/json",
   },
